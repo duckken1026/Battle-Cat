68889fa4f13c9b5ff0b7a7ed4c010585459fae9c
JimboMachine <100393854+JimboMachine@users.noreply.github.com>
2022-03-07 12:29:02 +0800

    Practice:8A

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -7,12 +7,29 @@
 #include<ctime>
 
 bool Guess(int number) {
-	return false;
+	static int = -1;
+
+	srand(time(NULL));
+	if (target == -1) {
+		target = rand() % 100 + 1;
+	}
+
+	if (number > target) {
+		std::cout << "Smaller" << std::endl;
+		return false;
+	}
+
+	else if (number < target) {
+		std::cout << "Bigger" << std::endl;
+		return false;
+	}
+	return true;
 }
 int main()
 {
 	int guess;
 	do {
+		std::cout << "Choose a number between 1-100:";
 		std::cin >> guess;
 	} while (!Guess(guess));
 	return 0;
